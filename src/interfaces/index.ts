333fc5944c1e603bea5138810eab8c8deea47ae3
Raphael Capeto <raphaelcapeto@gmail.com>
2021-05-07 21:55:26 -0300

    First Commit

---
+++ src/interfaces/index.ts
@@ -0,0 +1,23 @@
+export interface Comment {
+   id: number;
+   comment: string;
+   user: string;
+}
+
+export interface Room {
+   name: string;
+   price: string;
+   id: number;
+   description: string[];
+   images: string[];
+}
+
+export interface HARHotelProps {
+   gym: string;
+   main: string;
+   laundry: string;
+   bar: string;
+   restaurant: string;
+   spa: string;
+   pool: string;
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,12 @@
+import { HARHotelProps } from '../interfaces';
+import harMainImg from '../assets/har-hotel.png';
+
+export const HARHotel: HARHotelProps = {
+   pool: 'https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+   restaurant: 'https://images.unsplash.com/photo-1591920476948-da986641e66a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+   spa: 'https://images.unsplash.com/photo-1544161515-4ab6ce6db874?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+   bar: 'https://images.unsplash.com/photo-1597500320466-481a4518343b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+   laundry: 'https://images.unsplash.com/photo-1521656693074-0ef32e80a5d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+   main: harMainImg,
+   gym: 'https://images.unsplash.com/photo-1540497077202-7c8a3999166f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+}
